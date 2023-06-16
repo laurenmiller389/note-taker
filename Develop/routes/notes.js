c03490a6aebc33cb0046a5078c85fc0fff844a69
Lauren Miller <lmiller90@fordham.edu>
2023-06-16 18:53:02 -0400

    corrected notes js

---
+++ Develop/routes/notes.js
@@ -0,0 +1,40 @@
+const notes = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
+
+// GET Route for retrieving all the notes
+notes.get('/', (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+  });
+
+// GET Route for a specific note
+notes.get('/:id', (req, res) => {
+    res.json(JSON.parse(data)[req.params.id]);
+  });
+
+  // POST Route for a new note
+notes.post('/', (req, res) => {
+    //console.log(req.body);
+  
+    const { username, topic, tip } = req.body;
+  
+    if (req.body) {
+      const newNote = {
+        title,
+        text,
+        id: uuidv4(),
+      };
+  
+      readAndAppend(newNote, './db/db.json');
+      res.json(`Note added successfully 🚀`);
+    } else {
+      res.error('Error in adding note');
+    }
+  });
+
+  //bonus- delete
+  notes.delete('/:id', (req, res) => {
+
+  });
+
+  module.exports = notes;
--- Develop/server.js
+++ Develop/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const routes = require('./routes');
+const fs = require('fs');
+const path = require('path');
+const api = require('./routes');
 
 const PORT = process.env.PORT || 3001;
 
@@ -11,9 +13,53 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(routes);
+app.use('/api', api); // /api is new /routes
 
 app.use(express.static('public'));
+
+//we also need a get for the homepage '/' reg instance
+app.get('/', (req, res) => 
+    res.sendFile(path.join(__direname, '/public/index.html'))
+);
+
+//Get /notes to return notes.html
+app.get('/notes', (req, res) => 
+    res.sendFile(path.join(__direname, '/public/notes.html'))
+);
+//Get * to return index.html
+app.get('*', (req, res) => 
+    res.sendFile(path.join(__direname, '/public/index.html'))
+);
+
+
+
+//if we don't have a helper file/util in this project, we also need to put our readFile in here for the get and post /api
+app.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            const parsedData = JSON.parse(data);
+            res.json(parsedData);
+        }
+    });
+});
+
+//POST /api: get new note, save on req.body, add to db.json, return new note to client (writeFIle)
+app.post('api/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            const parsedData = JSON.parse(data);
+            const newNote = req.body;
+            parsedData.push(newNote);
+            fs.writeFile('./db/db.json')
+        }
+
+    });
+})
+
 
 //console log the url for the se4rver
 app.listen(PORT, () =>
